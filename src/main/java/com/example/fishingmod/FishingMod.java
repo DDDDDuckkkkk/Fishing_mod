2feb0969cb71180617c8916d9e4c68a5d78c7f38
DDDDDuckkkkk <7749078565ts@gmail.com>
2026-09-17 00:36:53 +0100

    Update FishingMod.java

--- src/main/java/com/example/fishingmod/FishingMod.java
+++ src/main/java/com/example/fishingmod/FishingMod.java
@@ -15,7 +15,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.FMLEnvironment;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -36,10 +36,6 @@ public class FishingMod {
             ITEMS.register("bass", () -> new Item(new Item.Properties().food(
                     new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build())));
 
-    public static final RegistryObject<SpawnEggItem> BASS_SPAWN_EGG =
-            ITEMS.register("bass_spawn_egg", () -> new SpawnEggItem(FISH.get(), 0x6B8E6B, 0x2F4F2F,
-                    new Item.Properties()));
-
     public static final RegistryObject<EntityType<LureEntity>> LURE =
             ENTITIES.register("lure", () -> EntityType.Builder.<LureEntity>of(LureEntity::new, MobCategory.MISC)
                     .sized(0.25F, 0.25F).clientTrackingRange(8).updateInterval(10).build("lure"));
@@ -47,6 +43,10 @@ public class FishingMod {
     public static final RegistryObject<EntityType<FishEntity>> FISH =
             ENTITIES.register("bass", () -> EntityType.Builder.of(FishEntity::new, MobCategory.WATER_CREATURE)
                     .sized(0.4F, 0.3F).clientTrackingRange(8).updateInterval(10).build("bass"));
+
+        public static final RegistryObject<SpawnEggItem> BASS_SPAWN_EGG =
+            ITEMS.register("bass_spawn_egg", () -> new SpawnEggItem(FISH.get(), 0x6B8E6B, 0x2F4F2F,
+                    new Item.Properties()));
 
     public FishingMod() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
